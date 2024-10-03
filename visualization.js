814d6fe71c58513fb00123452c69d828d1ca617a
Jose carlos Lopez <jlopez@dataseekers.es>
2024-10-03 16:01:21 +0000

    asdasd

--- visualization.js
+++ visualization.js
@@ -10,19 +10,20 @@ looker.plugins.visualizations.add({
       url_to_embed: {
         type: "string",
         label: "URL to embed",
-        default: "https://looker-viewer-dot-analysis-seeker.appspot.com/admin/calendar"
+        default: "http://127.0.0.1:9080/admin/calendar/"
       }
     },
 
     create: function(element, config) {
-      element.innerHTML == "
+      element.innerHTML = "
         <div id="custom-vis-container" style="width:100%; height:100%; overflow:auto;">
           <iframe id="embed-frame" src="${config.url_to_embed}" style="width:100%; height:100%; border:none;"></iframe>
         </div>";
+      element.style.color = config.text_color;
     },
 
     update: function(data, element, config, queryResponse) {
       document.getElementById("embed-frame").src = config.url_to_embed;
-      document.getElementById("embed-frame").style.color = config.text_color;
+      element.style.color = config.text_color;
     }
-  });
+});
