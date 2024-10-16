3ae0c0d29d8754ab8cf9e6537b3f91a9e877ab58
Jose carlos Lopez <jlopez@dataseekers.es>
2024-10-16 10:11:45 +0000

    hjgvj

--- visualization.js
+++ visualization.js
@@ -1,26 +1,33 @@
 looker.plugins.visualizations.add({
-    options: {
-      text_color: {
-        type: "string",
-        label: "Text Color",
-        default: "#000000"
-      },
-      url_to_embed: {
-        type: "string",
-        label: "URL to embed",
-        default: "https://looker-viewer-dot-analysis-seeker.appspot.com/admin/calendar/hotel-puentereal/2024-06-01/2025-12-31"
-      }
+  options: {
+    text: {},
+    text_color: {
+      type: "string",
+      label: "Text Color",
+      default: "#000000"
     },
+    url_to_embed: {
+      type: "string",
+      label: "URL to embed",
+      default: "https://looker-viewer-dot-analysis-seeker.appspot.com/admin/calendar/hotel-puentereal/2024-06-01/2025-12-31"
+    },
+    font_family: {
+      type: "string",
+      label: "Font Family",
+      default: "'Roboto', sans-serif"
+    }
+  },
 
-    create: function(element, config) {
-      element.innerHTML = `<div
-      id='custom-vis-container' style='width:100%; height:100%; overflow:auto;'>
+  create: function(element, config) {
+    element.innerHTML = `<div
+      id='custom-vis-container' style='width:100%; height:100%; overflow:auto; font-family: ${config.font_family};'>
       <iframe id='embed-frame' allow='fullscreen' sandbox='allow-scripts allow-forms' src='${config.url_to_embed}' style='width:100%; height:100%; border:none;'></iframe>
       </div>`;
-    },
+  },
 
-    update: function(data, element, config, queryResponse) {
-      document.getElementById("embed-frame").src = config.url_to_embed;
-      document.getElementById("embed-frame").style.color = config.text_color;
-    }
-  });
+  update: function(data, element, config, queryResponse) {
+    document.getElementById("embed-frame").src = config.url_to_embed;
+    document.getElementById("embed-frame").style.color = config.text_color;
+    document.getElementById("custom-vis-container").style.fontFamily = config.font_family;
+  }
+});
